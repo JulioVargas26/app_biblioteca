c03d2aaef434da3668b12eb6c062f535a98e9d60
DAMII <JulioVargas26>
2023-11-12 23:00:12 -0500

    apis

---
+++ app_biblioteca/Api/ApiAutor/ItemApiAutorTableViewCell.swift
@@ -0,0 +1,23 @@
+//
+//  ItemApiAutorTableViewCell.swift
+//  app_biblioteca
+//
+//  Created by julio vargas on 12/11/23.
+//
+
+import UIKit
+
+class ItemApiAutorTableViewCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
